d7e80dc514ab8d05e0fbe8de55fda236891e6701
lucnguyen1375 <luchuubang@gmai.com>
2024-10-24 00:01:40 +0700

    JAVA

--- src/BTVN_Buoi_1.java
+++ src/BTVN_Buoi_1.java
@@ -1,43 +1,44 @@
 import java.util.Scanner;
 
+public class Student{
+    String ten, tuoi, diaChi, hocLuc;
+    double diemToan, diemVan, diemAnh, diemTrungBinh;
+    Student(double diemToan, double diemVan, double diemAnh){
+        this.diemToan = diemToan;
+        this.diemVan = diemVan;
+        this.diemAnh = diemAnh;
+    }
+    void getDiemTrungBinh()
+    {
+        this.diemTrungBinh = (this.diemToan + this.diemVan + this.diemAnh)/3;
+    }
+
+    void getHocLuc() {
+        if (diemTrungBinh >= 8){
+            hocLuc = "Xuat sac";
+        }
+        else if (diemTrungBinh >= 7){
+            hocLuc = "Gioi";
+        }
+        else if (diemTrungBinh >= 6){
+            hocLuc = "Kha";
+        }
+        else if (diemTrungBinh >= 5){
+            hocLuc = "Trung binh";
+        }
+        else {
+            hocLuc = "Yeu";
+        }
+    }
+}
+
+public class StudentManager{
+    void inDiem(BTVN_Buoi_1.Student s) {
+        System.out.println(s.ten + " " + s.tuoi + " " + s.diaChi + " " + s.diemToan + " " + s.diemVan + " " + s.diemAnh + " " + s.diemTrungBinh + " " + s.hocLuc);
+    }
+}
 public class BTVN_Buoi_1 {
-    public class Student{
-        String ten, tuoi, diaChi, hocLuc;
-        double diemToan, diemVan, diemAnh, diemTrungBinh;
-        Student(double diemToan, double diemVan, double diemAnh){
-            this.diemToan = diemToan;
-            this.diemVan = diemVan;
-            this.diemAnh = diemAnh;
-        }
-        void getDiemTrungBinh()
-        {
-            this.diemTrungBinh = (this.diemToan + this.diemVan + this.diemAnh)/3;
-        }
 
-        void getHocLuc() {
-            if (diemTrungBinh >= 8){
-                hocLuc = "Xuat sac";
-            }
-            else if (diemTrungBinh >= 7){
-                hocLuc = "Gioi";
-            }
-            else if (diemTrungBinh >= 6){
-                hocLuc = "Kha";
-            }
-            else if (diemTrungBinh >= 5){
-                hocLuc = "Trung binh";
-            }
-            else {
-                hocLuc = "Yeu";
-            }
-        }
-    }
-
-    public class StudentManager{
-        void inDiem(Student s) {
-            System.out.println(s.ten + " " + s.tuoi + " " + s.diaChi + " " + s.diemToan + " " + s.diemVan + " " + s.diemAnh + " " + s.diemTrungBinh + " " + s.hocLuc);
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
